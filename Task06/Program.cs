42c10bb0ba0e5c2fab47ce3520236eb13fa00378
«AnastasiyaLebedeva» <lmehanik72@mail.ru>
2023-02-12 16:01:14 +0300

    дз

---
+++ Task06/Program.cs
@@ -0,0 +1,20 @@
+﻿//6: Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberDiv = number % 2;
+
+if(numberDiv == 0)
+{
+    Console.WriteLine($"{number}=> Да");
+}
+else
+{
+    Console.WriteLine($"{number}=> Нет");
+}
---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿//8: Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+
+int numberN = Convert.ToInt32(Console.ReadLine());
+int numberI = 2;
+
+if (numberN > 1)
+{
+    while (numberI <= numberN)
+    {
+        Console.Write(numberI + " ");
+        numberI = numberI + 2;
+    }
+}
